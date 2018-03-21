fc614c25fb60bd18f67ca56b64757ac0502b0d81
= <michaelwchastain@gmail.com>
2018-03-20 17:24:24 -0700

    moves strategy pattern to subdir and adds new demo

---
+++ strategyPattern/strategyDemo2.php
@@ -0,0 +1,94 @@
+<?php
+
+class testContext {
+    private $strategy = NULL; 
+    //bookList is not instantiated at construct time
+    public function __construct($strategy_ind_id) {
+        switch ($strategy_ind_id) {
+            case "Intra": 
+                $this->strategy = new Intra($strategy_ind_id);
+            break;
+            case "Snot": 
+                $this->strategy = new Snot($strategy_ind_id);
+            break;
+            case "MQ": 
+                $this->strategy = new MQ($strategy_ind_id);
+            break;
+            case "Prick": 
+                $this->strategy = new Prick($strategy_ind_id);
+            break;
+        }
+    }
+    public function printTest() {
+      return $this->strategy->printTest();
+    }
+}
+abstract class Test 
+{
+    protected $name;
+    public function __construct($name_in) 
+    {
+        $this->name = $name_in;
+    }
+}
+
+interface StrategyInterface {
+    public function printTest();
+}
+ 
+class Prick extends Test implements StrategyInterface {
+    public function printTest()
+    {
+        echo "I am the I am the" . "Very model of a Prick"  . ".\n";
+    }
+
+}
+
+class MQ extends Test implements StrategyInterface {
+    public function printTest()
+    {
+        echo "I am the I am the Very model of an MQ"  . ".\n";
+    }
+
+}
+class Intra extends Test implements StrategyInterface {
+    public function printTest()
+    {
+        echo "I am the I am the " . $this->name . "!!!\n";
+    }
+}
+
+class Snot extends Test implements StrategyInterface {
+    public function printTest()
+    {
+        echo "I am the Snot!!!\n";
+    }
+
+}
+
+  writeln('BEGIN TESTING STRATEGY PATTERN');
+  writeln('');
+ 
+  $strategyContextC = new testContext('Intra');
+  $strategyContextE = new testContext('Snot');
+  $strategyContextS = new testContext('MQ');
+ 
+  writeln('test 1 - show name context C');
+  writeln($strategyContextC->printTest());
+  writeln('');
+
+  writeln('test 2 - show name context E');
+  writeln($strategyContextE->printTest());
+  writeln('');
+ 
+  writeln('test 3 - show name context S');
+  writeln($strategyContextS->printTest());
+  writeln('');
+
+  writeln('END TESTING STRATEGY PATTERN');
+
+  function writeln($line_in) {
+    echo $line_in."\n";
+  }
+
+?>
